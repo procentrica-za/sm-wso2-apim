df7c66f78878754c94a2e9eb9307099a08bf06ce
Chane Bornman <chane@procentrica.co.za>
2020-07-07 12:33:19 +0200

    Committed site.json file to whitelist apim IP address

--- resources/scripts/keytool.sh
+++ resources/scripts/keytool.sh
@@ -1,1 +1,4 @@
-keytool -import -alias wso2carbon1 -file wso2carbon.cer -keystore client-truststore.jks -storepass wso2carbon
+#!/bin/bash
+
+keytool -import -alias wso2carbon1 -file /home/wso2carbon/wso2am-2.6.0/repository/conf/security/wso2carbon.cer -keystore client-truststore.jks -storepass wso2carbon
+echo "ran keytool"
